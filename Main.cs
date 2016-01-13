f325dad7c763b2c8bfeb1ac4a9396b508c4fe409
Jonathan Fricke <Jonathan.Fricke@student.lssc.edu>
2016-01-13 11:52:52 -0500

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sandbox.Common;
+using Sandbox.Common.ModAPI;
+using Sandbox.Game;
+using Sandbox.Game.Multiplayer;
+using VRage;
+using VRage.Library;
+
+namespace Project_Galaxies
+{
+    public class Main
+    {
+    }
+}
